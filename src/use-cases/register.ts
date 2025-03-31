c66eefde2d042a0fe93d19bd438223f20554fa59
gustavoeguedes <gustavoestevesguedes12@gmail.com>
2025-03-31 14:49:15 -0300

    implement user existence check with custom error handling in registration use case

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,6 +1,7 @@
 import { hash } from 'bcryptjs'
 import { prisma } from '@/lib/prisma'
 import { UsersRepository } from '../repositories/users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -16,7 +17,7 @@ export class RegisterUseCase {
    const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
-      throw new Error('User already exists')
+      throw new UserAlreadyExistsError()
     }
 
     await this.usersRepository.create({

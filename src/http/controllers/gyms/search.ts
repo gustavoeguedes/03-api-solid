a6c8bdd0187e5aea29771707eb784fe027b9dbb0
gustavoeguedes <gustavoestevesguedes12@gmail.com>
2025-04-06 11:00:59 -0300

    feat: implement gyms and users routes; add search, nearby, create, register, authenticate, and profile functionalities

---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { makeSearchGymsUseCase } from '../../../use-cases/factories/make-search-gyms-use-case'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsQuerySchema = z.object({
+    q: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { q, page } = searchGymsQuerySchema.parse(request.query)
+
+  const searchGymsUseCase = makeSearchGymsUseCase()
+  const { gyms } = await searchGymsUseCase.execute({
+    page,
+    query: q,
+  })
+
+  reply.status(200).send({
+    gyms,
+  })
+}
